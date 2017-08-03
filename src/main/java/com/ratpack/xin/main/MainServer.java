1303f0e0d9abc4f1163ada91d16efa3ddaadcc34
zhangxin <zhangxin@tvm.cn>
2017-08-03 17:54:11 +0800

    first commit

---
+++ src/main/java/com/ratpack/xin/main/MainServer.java
@@ -0,0 +1,10 @@
+package com.ratpack.xin.main;
+
+/**
+ * Created by zhangx on 2017/8/3.
+ */
+public class MainServer {
+    public static void main(String[] args) {
+        System.out.println("hello future");
+    }
+}
---
+++ src/main/java/com/ratpack/xin/pojo/UserInfo.java
@@ -0,0 +1,7 @@
+package com.ratpack.xin.pojo;
+
+/**
+ * Created by zhangx on 2017/8/3.
+ */
+public class UserInfo {
+}
