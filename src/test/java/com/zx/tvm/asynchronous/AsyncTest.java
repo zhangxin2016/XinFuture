d1f2b03f632c7be248104707f597ddb75d4b9dc8
zhangxin <zhangxin@tvm.cn>
2017-08-31 18:59:29 +0800

    add async

---
+++ src/test/java/com/zx/tvm/asynchronous/AsyncTest.java
@@ -0,0 +1,36 @@
+package com.zx.tvm.asynchronous;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Supplier;
+
+/**
+ * Created by zhangx on 2017/8/31
+ */
+public class AsyncTest {
+    public static void main(String[] args) throws Throwable, ExecutionException {
+        // 两个线程的线程池
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        //jdk1.8之前的实现方式
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(new Supplier<String>() {
+            @Override
+            public String get() {
+                System.out.println("task started!");
+                try {
+                    //模拟耗时操作
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "task finished!";
+            }
+        }, executor);
+
+        //采用lambada的实现方式
+        future.thenAccept(e -> System.out.println(e + " ok"));
+
+        System.out.println("main thread is running");
+    }
+}
